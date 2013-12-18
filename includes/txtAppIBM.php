e60e0288874c90d7ade6ccebbbddf648bc7c5341
Webmaster <glara.styleweb@gmail.com>
2013-12-17 18:25:03 -0600

    Cambio

---
+++ includes/txtAppIBM.php
@@ -0,0 +1,40 @@
+<?php
+	$txtApp = array();
+	//variables para la sesion
+	$txtApp['session']['name'] = "controlIBM";
+	$txtApp['session']['register'] = "controlIBM";
+	$txtApp['session']['nivelUsuario'] = "nivelUsuarioIBM";
+	$txtApp['session']['loginUsuario'] = "loginUsuarioIBM";
+	$txtApp['session']['passwordUsuario'] = "passwordUsuarioIBM";
+	$txtApp['session']['idUsuario'] = "idUsuarioIBM";
+	$txtApp['session']['nombreUsuario'] = "nombreUsuarioIBM";
+	$txtApp['session']['apellidoUsuario'] = "apellidoUsuarioIBM";
+	$txtApp['session']['origenSistemaUsuario'] = "origenSistemaUsuarioIBM";
+	$txtApp['session']['origenSistemaUsuarioNombre'] = "origenControlAlmacenOperaciones";
+	$txtApp['session']['cambiarPassUsuario'] = "cambiarPassUsuarioIBM";
+	$txtApp['session']['sexoUsuario'] = "sexoUsuarioIBM";
+	$txtApp['session']['nominaUsuario'] = "nominaUsuarioIBM";
+	$txtApp['session']['idProyectoSeleccionado'] = "idProyectoSeleccionado";
+	//login de usuarios
+	$txtApp['login']['tituloAppTitle'] = "[ App - Desarrollo ]";
+	$txtApp['login']['tituloAppPrincipal'] = "IQe. Sisco - IBM";
+	$txtApp['login']['tituloUsuario'] = "Usuario:";
+	$txtApp['login']['tituloPass'] = "Password:";
+	$txtApp['login']['btnApp'] = "Entrar";
+	$txtApp['login']['tituloAbout'] = "Acerca de...";
+	$txtApp['login']['pieLogin'] = "Acceso Intranet IQelectronics International ".date("Y");
+	//App GUI Principal
+	$txtApp['appPrincipal']['tituloPrincipalApp'] = "M&oacute;dulo Ingenieria IBM";
+	$txtApp['appPrincipal']['cerrarSesion'] = "Cerrar Sesi&oacute;n";
+	$txtApp['appPrincipal']['msgModulo'] = "Ingenieria IBM";
+	$txtApp['appPrincipal']['msgActualizaciones'] = "Actualizaciones";
+	$txtApp['appPrincipal']['msgReportarError'] = "Reportar Problema";
+	$txtApp['appPrincipal']['msgBarraCarga'] = "Listo";
+	//Mensajes de error
+	$txtApp['mensajeError']['salirApp'] = "Si decide continuar, se pueden perder los cambios que no alla Guardado.";
+	//Pagina Profile
+	$txtApp['appProfile']['tituloPagina'] = "Informaci&oacute;n del Perfil";
+	$txtApp['appProfile']['tituloModulo'] = "Perfil de Usuario";
+	//Nombre del Sistema
+	$txtApp['appSistema']['nombreSistemaActual'] = "IBM";
+?>
